95c664732805990573345e64377b8686a2310b71
Csampai Elemér <csampaie.5@gmail.com>
2021-12-16 14:57:24 +0100

    Post törlése (delete own content)

--- python_cms/static/create_post.js
+++ python_cms/static/create_post.js
@@ -7,11 +7,3 @@ document.querySelector('#createPost').addEventListener('click', function (e) {
   }
 });
 
-// if there are toasts visible
-// https://picturepan2.github.io/spectre/components/toasts.html
-// this will make it dissapear, if the user clicks on the X
-const toasts = document.querySelectorAll('.toast button');
-
-toasts.forEach(el => el.addEventListener('click', event => {
-  event.target.closest('.toast').remove()
-}));
